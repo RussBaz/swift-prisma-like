7f439ade5b5cfd6227dcfb0872e02260abc229a4
RussBaz <RussBaz@users.noreply.github.com>
2024-05-23 17:33:47 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,59 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-prisma-like",
+    platforms: [
+        .macOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "swift-prisma-runtime",
+            targets: ["Runtime"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0"),
+        .package(url: "https://github.com/vapor/sql-kit.git", from: "3.30.0"),
+        .package(url: "https://github.com/vapor/sqlite-kit.git", from: "4.5.1"),
+    ],
+    targets: [
+        .target(name: "Core", swiftSettings: swiftSettings),
+        .executableTarget(
+            name: "Example",
+            dependencies: [
+                .product(name: "SQLKit", package: "sql-kit"),
+                .product(name: "SQLiteKit", package: "sqlite-kit"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+        .target(name: "Plugin", swiftSettings: swiftSettings),
+        .target(name: "Runtime", swiftSettings: swiftSettings),
+        .executableTarget(
+            name: "Tool",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+        .testTarget(
+            name: "SwiftPrismaLikeTests",
+            dependencies: ["Core"],
+            swiftSettings: swiftSettings
+        ),
+    ]
+)
+
+var swiftSettings: [SwiftSetting] { [
+    .enableUpcomingFeature("BareSlashRegexLiterals"),
+    .enableUpcomingFeature("ConciseMagicFile"),
+    .enableUpcomingFeature("ForwardTrailingClosures"),
+    .enableUpcomingFeature("ImportObjcForwardDeclarations"),
+    .enableUpcomingFeature("DisableOutwardActorInference"),
+    .enableUpcomingFeature("ExistentialAny"),
+    .enableUpcomingFeature("DeprecateApplicationMain"),
+    .enableUpcomingFeature("GlobalConcurrency"),
+    .enableUpcomingFeature("IsolatedDefaultValues"),
+    .enableExperimentalFeature("StrictConcurrency"),
+] }
